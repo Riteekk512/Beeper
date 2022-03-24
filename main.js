7d9de3df6edb8bdb3e6c5649fb8670a42ceb5c49
Riteekk512 <102307546+Riteekk512@users.noreply.github.com>
2022-03-24 20:04:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+song = "";
+status = "";
+objects = [];
+
+function preload() {
+  song = loadSound("1.mp3");
+
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(640, 420);
+    video.hide();
+        objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "status:Detecting Object";
+
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+    if (status != "") {
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : object dection";
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            if(objects[i].label="person"){
+                document.getElementById("number_of_objects").innerHTML="Person detected";
+                console.log("stop");
+                song.stop();
+                }
+                else{
+                    document.getElementById("number_of_objects").innerHTML="Person not detected";
+                    console.log("play");
+                    song.play();
+                }
+
+
+        }
+    }
+}
+
+function modelLoaded() {
+    console.log("modelLoaded");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
